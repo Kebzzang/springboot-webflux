101a9a1562d5255820837c00777b6d25ccfc64ef
Kebzzang <kebinkorea@gmail.com>
2021-03-24 00:50:38 +0900

    sample code practiced

---
+++ flux-demo/src/main/java/com/keb/fluxdemo/domain/DBinit.java
@@ -0,0 +1,66 @@
+package com.keb.fluxdemo.domain;
+
+import io.r2dbc.spi.ConnectionFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+
+import java.time.Duration;
+import java.util.Arrays;
+@Configuration
+@Slf4j
+public class DBinit {
+    @Bean
+    ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        initializer.setDatabasePopulator(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
+
+        return initializer;
+    }
+
+    @Bean
+    public CommandLineRunner demo(CustomerRepository repository) {
+
+        return (args) -> {
+            // save a few customers
+            repository.saveAll(Arrays.asList(new com.keb.fluxdemo.domain.Customer("Jack", "Bauer"),
+                    new com.keb.fluxdemo.domain.Customer("Chloe", "O'Brian"),
+                    new com.keb.fluxdemo.domain.Customer("Kim", "Bauer"),
+                    new com.keb.fluxdemo.domain.Customer("David", "Palmer"),
+                    new com.keb.fluxdemo.domain.Customer("Michelle", "Dessler")))
+                    .blockLast(Duration.ofSeconds(10));
+
+            // fetch all customers
+            log.info("Customers found with findAll():");
+            log.info("-------------------------------");
+            repository.findAll().doOnNext(customer -> {
+                log.info(customer.toString());
+            }).blockLast(Duration.ofSeconds(10));
+
+            log.info("");
+
+            // fetch an individual customer by ID
+            repository.findById(1L).doOnNext(customer -> {
+                log.info("Customer found with findById(1L):");
+                log.info("--------------------------------");
+                log.info(customer.toString());
+                log.info("");
+            }).block(Duration.ofSeconds(10));
+
+
+            // fetch customers by last name
+            log.info("Customer found with findByLastName('Bauer'):");
+            log.info("--------------------------------------------");
+            repository.findByLastName("Bauer").doOnNext(bauer -> {
+                log.info(bauer.toString());
+            }).blockLast(Duration.ofSeconds(10));;
+            log.info("");
+        };
+    }
+}
---
+++ flux-demo/src/test/java/com/keb/fluxdemo/FluxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keb.fluxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
